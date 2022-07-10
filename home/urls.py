b9227b0dffe9f7f53b72ed07bd6a4a33a8d937b3
Amid <azorty71@gmail.com>
2022-07-10 22:04:15 +0300

    add js

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path 
 from home.views import index, about, avtor,contakt
-
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', index, name = "index"),
@@ -9,3 +9,7 @@ urlpatterns = [
     path('avtor/', avtor, name = "avtor"),
     path('contakt/', contakt, name = "contakt"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
