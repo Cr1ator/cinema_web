e65cf5de8c262aa706f22cad283fe7cd419d5a5a
Amid <azorty71@gmail.com>
2022-07-10 18:07:51 +0300

    add categories

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from categories.models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug" : ("title", )}
+    admin.site.register(Category)
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from categories.models import Category
+from movies.models import Movie
+from home.models import Setting
+from django.core.paginator import Paginator
 
 # Create your views here.
+
+def category_detail(request, slug):
+    category = Category.objects.get(slug = slug)
+    categories = Category.objects.all().order_by('?')[:8]
+    home = Setting.objects.latest('-id')
+    movies = Movie.objects.all().order_by('-id')
+    paginator = Paginator(movies, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'category' : category,
+        'movies' : movies,
+        'home' : home,
+        'categories' : categories,
+        'page_obj' : page_obj,
+    }
+    return render(request, 'index.html', context)
